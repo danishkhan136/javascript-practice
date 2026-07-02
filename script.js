cbfe977a8673213b4b96d46c5da6ba34e93245ec
danishkhan136 <21sw136@students.muet.edu.pk>
2026-07-02 15:55:21 +0500

    commit 42

--- script.js
+++ script.js
@@ -981,9 +981,22 @@
 
 // let count=10
 
+// example
+
 // setInterval(function(){
 //     if(count>=1){
 //     count--;
 //     console.log(count);
 //     }
 // },1000);
+
+
+let interval = setInterval(function(){
+    if(count>=1){
+    count--;
+    console.log(count);
+    }
+    else{
+        clearInterval(interval);
+    }
+},1000);
