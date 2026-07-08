a922474e2e4cbd6ea88fa522d17ac6318e0dbeda
danishkhan136 <21sw136@students.muet.edu.pk>
2026-07-08 10:29:25 +0500

    commit 47

---
+++ script2.js
@@ -0,0 +1,7 @@
+// localstorage -> apka browser ka andader jo data store karna jo ki browser band hpna per bhi delete nahi hoga
+
+// sessionstorage -> apka data temporarily store kar karta hai matlab ki tab band hua aur data gaya
+
+// cookies -> ye bhi data store karta hai aur aapka data browser ka cookies naamki property ma save hota hai aur ye cookie concept kam data ya light data ke liye hota hai
+
+// 08/07/2026
